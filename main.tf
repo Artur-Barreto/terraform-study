c248625730b29cde5b3f0c3054cbea240b78aefb
Artur-Barreto <artur.barreto7@outlook.com>
2023-03-20 18:36:38 -0300

    Iniciando o projeto

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-02f3f602d23f1659d" 
+  ami           = "ami-02f3f602d23f1659d"
   instance_type = "t2.micro"
 
   tags = {
